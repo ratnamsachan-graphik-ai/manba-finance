d982d706425bb02301703d0a62edceda8e3126bf
Ratnam Sachan <ratnam@vecton.ai>
2026-01-20 09:58:03 +0000

    change the favicon to the logo i provided and the meat adata also to pro

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,11 @@ import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
 
 export const metadata: Metadata = {
-  title: 'WHFL Welcome Call',
+  title: 'Prosperity Finance',
   description: 'This is a demo link and should only be used for demonstration purposes.',
+  icons: {
+    icon: 'https://ik.imagekit.io/wekwjyn8z/prosperity-finance-logo.webp',
+  },
 };
 
 export default function RootLayout({
@@ -14,8 +17,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="light">
-      <head>
-      </head>
       <body className="antialiased text-[13px]">
         {children}
         <Toaster />
